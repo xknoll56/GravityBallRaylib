cf1a2a003563c5725577a80515b797ad755dafb1
Xavier Knoll <xavierknoll56@gmail.com>
2026-06-22 13:21:15 -0700

    add raylib scene

--- GravityBallRaylib/main.cpp
+++ GravityBallRaylib/main.cpp
@@ -1,6 +1,122 @@
-#include <iostream>
+﻿/*******************************************************************************************
+*
+*   raylib [core] example - 3d camera free
+*
+*   Example complexity rating: [★☆☆☆] 1/4
+*
+*   Example originally created with raylib 1.3, last time updated with raylib 1.3
+*
+*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
+*   BSD-like license that allows static linking with closed source software
+*
+*   Copyright (c) 2015-2025 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+#include "GBSimulation.h"
+#include "raylib.h"
+#include "raymath.h"
 
-void main()
+Vector3 toRayVec(GBVector3 v)
 {
-	std::cout << "Hello World" << std::endl;
+    return { v.y, v.z, -v.x };
+}
+
+Quaternion toRayQuat(GBQuaternion q)
+{
+    return { q.y, q.z, -q.x, q.w };
+}
+
+Matrix makeTransform(
+    GBVector3 position,
+    GBQuaternion rotation,
+    GBVector3 scale)
+{
+    Vector3 rayPos = toRayVec(position);
+    Vector3 rayScale = toRayVec(scale);
+    Matrix S = MatrixScale(rayScale.x, rayScale.y, rayScale.z);
+    Matrix R = QuaternionToMatrix(toRayQuat(rotation));
+    Matrix T = MatrixTranslate(rayPos.x, rayPos.y, rayPos.z);
+
+    return MatrixMultiply(
+        MatrixMultiply(S, R),
+        T
+    );
+}
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 1024;
+    const int screenHeight = 768;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera free");
+
+    // Define the camera to look into our 3d world
+    Camera3D camera = { 0 };
+    camera.position = { 10.0f, 10.0f, 10.0f }; // Camera position
+    camera.target = { 0.0f, 0.0f, 0.0f };      // Camera looking at point
+    camera.up = { 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+    camera.fovy = 45.0f;                                // Camera field-of-view Y
+    camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
+
+    GBVector3 cubePosition = { 0.0f, 10.0f, 10.0f };
+    GBQuaternion cubeRotation;
+
+    Mesh cubeMesh = GenMeshCube(1.0f, 1.0f, 1.0f);
+    Model cubeModel = LoadModelFromMesh(cubeMesh);
+
+
+    DisableCursor();                    // Limit cursor to relative movement inside the window
+
+    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())        // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        UpdateCamera(&camera, CAMERA_FREE);
+
+        if (IsKeyPressed(KEY_Z)) camera.target = { 0.0f, 0.0f, 0.0f };
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        BeginMode3D(camera);
+
+        cubeRotation = cubeRotation * GBQuaternion::fromAxisAngle(GBVector3::forward(), 0.01f);
+        cubeModel.transform = makeTransform(cubePosition, cubeRotation, { 1,1,1 });
+        DrawModel(cubeModel,{0,0,0}, 1.0f, BLUE);
+
+        DrawGrid(10, 1.0f);
+
+        EndMode3D();
+
+        DrawRectangle(10, 10, 320, 93, Fade(SKYBLUE, 0.5f));
+        DrawRectangleLines(10, 10, 320, 93, BLUE);
+
+        DrawText("Free camera default controls:", 20, 20, 10, BLACK);
+        DrawText("- Mouse Wheel to Zoom in-out", 40, 40, 10, DARKGRAY);
+        DrawText("- Mouse Wheel Pressed to Pan", 40, 60, 10, DARKGRAY);
+        DrawText("- Z to zoom to (0, 0, 0)", 40, 80, 10, DARKGRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
 }
